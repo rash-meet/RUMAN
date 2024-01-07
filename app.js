b468180dcca5024c1780aeba82c310a01475aa79
RASHMEET SINGH <rashmeet5304@gmail.com>
2024-01-08 01:29:05 +0530

    semifinal

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+// function redirectToTeacherPage() {
+//     window.location.href = "teacher.html";
+// }
 window.addEventListener('resize', function(){
     if (document.documentElement.clientWidth < 1200) {
         let x = document.querySelector("h1");
@@ -8,6 +11,24 @@ window.addEventListener('resize', function(){
         x.innerHTML = '<a href="/index.html">The Stanford Natural Language Processing Group</a>'; 
     }
     });
+//     function redirectToTeacherPage() {
+//     window.location.href = "teacher.html";
+// }
+    function changeColour(value)
+    {
+        const myElement = document.getElementById("myVideo");
+        var color = document.body.style.backgroundColor;
+        switch(value)
+        {
+            case 'm':
+                myElement.src="ruman male teacher - Made with Clipchamp.mp4";
+            break;
+            case 'f':
+                myElement.src="Untitled video - Made with Clipchamp (5).mp4";
+            break;
+        }
+        // document.body.style.backgroundColor = color;
+    }
   if (document.documentElement.clientWidth < 1200) {
       let x = document.querySelector("h1");
       x.innerHTML = '<a href="/index.html">The Stanford NLP Group</a>';
